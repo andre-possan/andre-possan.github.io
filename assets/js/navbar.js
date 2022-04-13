06451ab12a37acfd73509bea260eaf28caafe811
andre <andrecorsopossan@gmail.com>
2022-04-13 17:58:28 -0300

    Header update

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,7 +1,7 @@
+const header = document.querySelector(".header");
 const mobileMenu = document.querySelector(".header__mobile-menu");
 const navList = document.querySelector(".header__nav");
 const navItens = document.querySelectorAll(".header__item");
-
 
 let open = false;
 function openMenu(navList) {
@@ -28,7 +28,32 @@ function addItensAnimation(itens) {
     });
 }
 
+function changeNavBackground(attribute){
+    if(attribute == "add") {
+        header.classList.add("header--highlight");
+        navList.classList.add("header__nav--highlight");
+    } else {
+        header.classList.remove("header--highlight");
+        navList.classList.remove("header__nav--highlight");
+    }
+}
+
 mobileMenu.addEventListener("click", () => {
     openMenu(navList)
     addItensAnimation(navItens);
+    if(open == true) {
+        changeNavBackground("add");
+    } else {
+        if(window.scrollY < 20) {
+            changeNavBackground("remove");
+        }
+    }
+})
+
+window.addEventListener("scroll", ()=> {
+    if(window.scrollY > 20) {
+        changeNavBackground("add");
+    } else if(open == false) {
+        changeNavBackground("remove");
+    }
 })
