e9bf488883a675034be63dd13a1b88af22430c2b
andre <andrecorsopossan@gmail.com>
2022-05-15 17:04:13 -0300

    Add animations on page

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,41 +1,100 @@
-const informationCards = document.querySelectorAll(".cards .card");
-const informationListCards = document.querySelectorAll(".cards");
+const listCards = document.querySelectorAll(".cards");
+const cards = document.querySelectorAll(".cards .card");
+const texts = document.querySelectorAll(".title--highlight, .paragraph");
+const dividers = document.querySelectorAll(".divider");
 
-const directions = ["right","top","left"];
+const circleTest = document.querySelector(".circle-test");
+const circleTest2 = document.querySelector(".circle-test2");
 
-window.addEventListener("load", ()=> {
+const directions = ["right", "top", "left"];
+
+window.addEventListener("load", () => {
     addAnimations();
 })
 
-window.addEventListener("scroll", ()=> {
+window.addEventListener("scroll", () => {
     addAnimations();
 })
 
-function addAnimationOnCards(listsOfCards){
+function detectSection(section) {
+    const checkpoint = window.pageYOffset + window.innerHeight;
+    const checkpoint2 = window.pageYOffset;
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const checkpointStart = sectionTop <= checkpoint;
+    const checkpointEnd = sectionTop >= checkpoint2;
+
+    circleTest.style.top = `${checkpoint - 64}px`;
+    circleTest2.style.top = `${checkpoint2}px`;
+
+    if (checkpointStart && checkpointEnd) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function addAnimationOnCards(listsOfCards) {
     listsOfCards.forEach(listOfCards => {
         const cards = listOfCards.querySelectorAll(".card");
 
         for (let i = 0; i < cards.length; i++) {
-            const position = listOfCards.getBoundingClientRect()["y"];
             const card = cards[i];
             const direction = directions[i];
-            const parameter = 500;
-    
-            if (position >= parameter) {
-                if(direction == "right") {
-                    card.style.transform = `translate(${(- position + parameter) / 5}%)`;
-                } else if(direction == "left") {
-                    card.style.transform = `translate(${(position - parameter) / 5}%)`;
-                } else {
-                    card.style.transform = `translateY(${(position - parameter) / 5}%)`;
-                }
+
+            if (detectSection(card.parentNode) && direction == "right") {
+                card.classList.add("card--animate-to-right");
             } else {
-                card.style.transform = "";
+                card.classList.remove("card--animate-to-right");
+            }
+
+            if (detectSection(card.parentNode) && direction == "left") {
+                card.classList.add("card--animate-to-left");
+            } else {
+                card.classList.remove("card--animate-to-left");
+            }
+
+            if (detectSection(card.parentNode) && direction == "top") {
+                card.classList.add("card--animate-to-top");
+            } else {
+                card.classList.remove("card--animate-to-top");
             }
         }
     });
 }
 
-function addAnimations(){
-    addAnimationOnCards(informationListCards, informationCards);
+function addAnimationOnTexs(texts) {
+    for (let i = 0; i < texts.length; i++) {
+        const text = texts[i];
+
+        if (detectSection(text.parentNode) && text.tagName == "P") {
+            text.classList.add("paragraph--animate");
+        } else {
+            text.classList.remove("paragraph--animate");
+        }
+
+        if (detectSection(text.parentNode) && text.tagName == "H1") {
+            text.classList.add("title--highlight--animate");
+        } else {
+            text.classList.remove("title--highlight--animate");
+        }
+    }
+}
+
+function addAnimationOnDividers(dividers) {
+    for (let i = 0; i < dividers.length; i++) {
+        const divider = dividers[i];
+
+        if (detectSection(divider.parentNode)) {
+            divider.classList.add("divider--animation");
+        } else {
+            divider.classList.remove("divider--animation");
+        }
+    }
+}
+
+function addAnimations() {
+    addAnimationOnCards(listCards, cards);
+    addAnimationOnTexs(texts);
+    addAnimationOnDividers(dividers);
 }
