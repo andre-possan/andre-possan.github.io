72280143bb82f42142ba9c04eddaf54912881b1a
andre <andrecorsopossan@gmail.com>
2022-05-15 00:01:13 -0300

    Add animation on projects cads

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,5 +1,5 @@
-const informationCards = document.querySelectorAll(".information .cards .card");
-const informationListCards = document.querySelector(".information .cards");
+const informationCards = document.querySelectorAll(".cards .card");
+const informationListCards = document.querySelectorAll(".cards");
 
 const directions = ["right","top","left"];
 
@@ -11,26 +11,31 @@ window.addEventListener("scroll", ()=> {
     addAnimations();
 })
 
-function addAnimationOnCards(listOfCards, cards, parameter){
-    for (let i = 0; i < cards.length; i++) {
-        const position = listOfCards.getBoundingClientRect()["y"];
-        const card = cards[i];
-        const direction = directions[i];
-        
-        if (position >= parameter) {
-            if(direction == "right") {
-                card.style.transform = `translate(${(- position + parameter) / 5}%)`;
-            } else if(direction == "left") {
-                card.style.transform = `translate(${(position - parameter) / 5}%)`;
+function addAnimationOnCards(listsOfCards){
+    listsOfCards.forEach(listOfCards => {
+        const cards = listOfCards.querySelectorAll(".card");
+
+        for (let i = 0; i < cards.length; i++) {
+            const position = listOfCards.getBoundingClientRect()["y"];
+            const card = cards[i];
+            const direction = directions[i];
+            const parameter = 500;
+    
+            if (position >= parameter) {
+                if(direction == "right") {
+                    card.style.transform = `translate(${(- position + parameter) / 5}%)`;
+                } else if(direction == "left") {
+                    card.style.transform = `translate(${(position - parameter) / 5}%)`;
+                } else {
+                    card.style.transform = `translateY(${(position - parameter) / 5}%)`;
+                }
             } else {
-                card.style.transform = `translateY(${(position - parameter) / 5}%)`;
+                card.style.transform = "";
             }
-        } else {
-            card.style.transform = "";
         }
-    }
+    });
 }
 
 function addAnimations(){
-    addAnimationOnCards(informationListCards, informationCards, 500);
+    addAnimationOnCards(informationListCards, informationCards);
 }
